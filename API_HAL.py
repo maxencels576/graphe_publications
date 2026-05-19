aabc04ed966a7974182f7b5708a4af66cf0efb63
xio <xi_c_u@serveur.com>
2026-05-19 17:04:19 +0200

    ajout fonctionnalités des requêtes de l'API avec les noms du json contenant les enseignants et unicité des articles, avec HAL

--- API_HAL.py
+++ API_HAL.py
@@ -2,20 +2,36 @@ import requests
 import json
 
 startUrl = "https://api.archives-ouvertes.fr/search/?q=("
-endUrl = ")"
-ensName = "flavien vernier"
 
-data = []
+ensData = []
 
-response = requests.get(startUrl+ensName+endUrl)
-infos = response.json()
+try:
+    with open('enseignants.json', 'r') as file:
+        ensData = json.load(file)
 
-for elt in infos["response"]["docs"]:
-    elt = elt["label_s"].split(". ")
-    coAuth = elt[0].split(", ")
-    
-    data.append({"article":elt[1],"authors":coAuth})
+    articlesData = []
+
+    for ensJSON in ensData:
+
+        response = requests.get(startUrl+ensJSON["nom"]+")")
+        infos = response.json()
+
+        for elt in infos["response"]["docs"]:
+            elt = elt["label_s"].split(". ")
+            articleName = elt[1]
+
+            exists = False
+
+            for articleJSON in articlesData:
+                if articleName==articleJSON["article"]:
+                    exists = True
+            if not exists:
+                coAuth = elt[0].split(", ")
+                articlesData.append({"article":articleName,"authors":coAuth})
+
+    with open("articlesCoAuteurs_HAL.json", "wt+", newline="", encoding="utf-8") as f:
+        json.dump(articlesData, f, ensure_ascii=False, indent=4)
 
 
-with open("articlesCoAuteurs_HAL.json", "wt+", newline="", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+except FileNotFoundError:
+    print("Error: Fichier 'enseignants.json'.")
